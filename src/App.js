c18b788bb1f9339fbe03137604a9f3c7c4a4062f
Thracian42 <dvasiljevic45@yahoo.com>
2021-11-06 10:36:11 +0100

    dodao navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import './App.css'
 
 const App = () => {
     return (
         <div>
-            <h2>asd</h2>
-        </div>
+ <header>
+  <nav className="navbar navbar-expand-lg">
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content" aria-controls="navbar-content" aria-expanded="false" aria-label="toggle-navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+      <div className="collapse navbar-collapse" id="navbar-content">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Products</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Contacts</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Blogs</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div className="box">
+        <div className="box-1"></div>
+        <div className="box-2"></div>
+      </div>
+    </header>
+    <section>
+      <h1 className="text-center">Hello..!!!</h1>
+    </section>
+  </div>
     )
 }
 
